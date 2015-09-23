36b72d2eb816f16cb95e06a556bc3a22bd97a6f3
joelpinheiro <joelpinheiro@ua.pt>
2015-09-23 17:23:28 +0100

    Minor change

--- MakeDoc.sh
+++ MakeDoc.sh
@@ -1,5 +1,5 @@
 
-#!/bin/bash
+#!/usr/bin/env bash
 echo "Generating documentation..."
 JSDocLib/node_modules/.bin/jsdoc -d Documentation src/*
 echo "Documentation generated in 'Documentation' directory"
