c20149f6adb7a795383a617e2e5420ae8b82772d
Vitor Silva <vitorsilva@boldint.com>
2015-10-16 15:45:16 +0100

    update package.json to distribute RuntimeUA and Sandbox like a module

---
+++ src/runtime-core.js
@@ -0,0 +1,9 @@
+import RuntimeUA from 'runtime/RuntimeUA';
+import Sandbox from 'sandbox/Sandbox';
+
+let RuntimeCore = {
+  RuntimeUA: RuntimeUA,
+  Sandbox: Sandbox
+};
+
+export default RuntimeCore;
