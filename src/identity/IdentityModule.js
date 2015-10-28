189fa409926d5b74131de0dda02f52edda044e18
Kevin CORRE <kevin1.corre@orange.com>
2015-10-28 15:41:48 +0100

    Added outward API stub as in D4.1

--- src/identity/IdentityModule.js
+++ src/identity/IdentityModule.js
@@ -1,28 +1,80 @@
 /**
- * Functions to deal with assertions compliant with WebRTC RTCIdentityProvider
+ * IdentityModule
+ *
+ * Initial specification: D4.1
+ *
+ * The IdentityModule is a component managing user Identity. It downloads, instantiates
+ * and manage Identity Provider Proxy (IdP) for its own user identity or for external
+ * user identity verification. 
+ * 
  */
-class IdentityModule {
+var IdentityModule = {
+
+/**
+ *          USER'S OWN IDENTITY
+ */
 
   /**
-   * Generates an Identity Assertion
+   * Register a new Identity with an Identity Provider
+   */
+  registerIdentity: function(){
+    // Body...
+  },
+  
+  /**
+   * In relation with a classical Relying Party: Registration
+   */
+  registerWithRP: function() {
+    // Body...
+  },
+  
+  
+  /**
+   * In relation with a classical Relying Party: Login
+   */
+  loginWithRP: function() {
+    // Body...
+  },
+  
+  
+  /**
+   * In relation with a Hyperty Instance: Associate identity
+   */
+  setHypertyIdentity: function() {
+    // Body...
+  },
+  
+  /**
+   * Generates an Identity Assertion for a call session
    * @param  {DOMString} contents     contents
    * @param  {DOMString} origin       origin
    * @param  {DOMString} usernameHint usernameHint
    * @return {IdAssertion}              IdAssertion
    */
-  generateAssertion(contents, origin, usernameHint) {
+  generateAssertion: function(contents, origin, usernameHint) {
     // Body...
-  }
+  },
 
+
+/**
+ *          OTHER USER'S IDENTITY
+ */
+  
   /**
-   * Validates an Identity Assertion
+   * Verification of a received IdAssertion validity
    * @param  {DOMString} assertion assertion
-   * @param  {DOMString} origin    origin
    */
-  validateAssertion(assertion, origin) {
+  validateAssertion: function(assertion) {
     // Body...
-  }
-
+  },
+  
+  /**
+   * Trust level evaluation of a received IdAssertion
+   * @param  {DOMString} assertion assertion
+   */
+  getAssertionTrustLevel: function(assertion) {
+    // Body...
+  },
 }
 
-export default IdentityModule;
+module.exports = IdentityModule
