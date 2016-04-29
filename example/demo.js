94fa6f44bb5d387ea1766ae56529b11f275698a7
fbeierle <beierle@tu-berlin.de>
2016-04-29 11:58:05 +0200

    updated demo

--- example/demo.js
+++ example/demo.js
@@ -31,6 +31,19 @@ loginBtn.addEventListener('click', function(e) {
   let content = loginPanel.querySelector('.card-action');
   addLoader(content);
 
+  // Address Book Functionalities
+  runtime.graphConnector.addContact('nm0_J8Xpn74sdBBfV6Au7jsI12whtvd9PuJplCe7Yg8', 'John', 'Doe');
+  runtime.graphConnector.addContact('HCnzDTY51uwsl1_NZdoJnYRAiBowDKXqNN7oOX6TLuc', 'Alice', 'Apple');
+  runtime.graphConnector.addContact('HmDLrX7RgrZc28RHwNGIffcckIEbbu1vCBKYoyVPOSQ', 'Charlie', 'Miller');
+  runtime.graphConnector.removeContact('nm0_J8Xpn74sdBBfV6Au7jsI12whtvd9PuJplCe7Yg8');
+  console.log(runtime.graphConnector.checkGUID('HCnzDTY51uwsl1_NZdoJnYRAiBowDKXqNN7oOX6TLuc'));
+  console.log(runtime.graphConnector.getContact('Alice'));
+  runtime.graphConnector.calculateBloomFilter1Hop();
+  runtime.graphConnector.addUserID('http://example.com/Bob');
+  runtime.graphConnector.addUserID('http://serviceproviderx.com/Bobby');
+  runtime.graphConnector.removeUserID('http://serviceproviderx.com/Bobby');
+  
+  // GUID generation and contacting the Global Registry
   console.log('Generate GUID, returning mnemonic:');
   console.log(runtime.graphConnector.generateGUID());
   console.log('GUID:')
@@ -39,7 +52,47 @@ loginBtn.addEventListener('click', function(e) {
   console.log('Global Registry Record (JWT):')
   console.log(jwt);
   console.log('Sending Gloal Registry Record:')
-  runtime.graphConnector.sendGlobalRegistryRecord(jwt);
 
+  var sendPromise = runtime.graphConnector.sendGlobalRegistryRecord(jwt);
+  sendPromise.then(function (result) {
+	console.log('Result for sending the Global Registry Record');
+    console.log(result);
+	queryGlobalRegistry();
+  }, function (error) {
+    console.error('Error querying Global Registry: ', error);
+  });
+  
 });
 
+
+// querying the Global Registry
+function queryGlobalRegistry() {
+
+  console.log('Query Global Registry');
+  
+  var queryPromise = runtime.graphConnector.queryGlobalRegistry('f1CuNFhbYbPIR_Ng0GbtncTjoPGPfULY_p9J2nLX7bw');
+  queryPromise.then(function (result) {
+	console.log('Result for querying the Global Registry:');
+    console.log(result);
+	reuseGUID();
+  }, function (error) {
+    console.error('Error querying Global Registry: ', error);
+  });
+
+}
+
+
+// reusing an existing GUID by entering the mnemonic
+function reuseGUID() {
+
+  console.log('reuse GUID');
+  
+  var reuseGUIDPromise = runtime.graphConnector.useGUID('bag silly differ uncover later outer roast tail tissue naive dinner utility that winter assume ball');
+  reuseGUIDPromise.then(function (result) {
+	console.log('Result for reusing an existing GUID:');
+    console.log(result);
+  }, function (error) {
+    console.error('Error querying Global Registry: ', error);
+  });
+
+}
