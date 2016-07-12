132595a4daceb4f4bf382ca6a3eda19c0bebb2f6
ishan210788 <ishan210788@gmail.com>
2016-07-12 15:29:28 +0200

    add/remove userID and getting all contacts

--- src/graphconnector/GraphConnector.js
+++ src/graphconnector/GraphConnector.js
@@ -373,6 +373,7 @@ class GraphConnector {
   /**
    * Adds a UserID for the user.
    * @param  {string}     userID          The UserID for a Domain Registry to add for the user.
+   * @returns  {boolean}   true if the userID exists and the user ID will not be added, false otherwise.
    */
   addUserID(userID) {
     // check if already inside
@@ -385,18 +386,23 @@ class GraphConnector {
     if (!found) {
       this.globalRegistryRecord.userIDs.push(userID);
     }
+    return found;
   }
 
   /**
    * Removes a UserID for the user.
    * @param  {string}     userID          The UserID to remove.
+   * @returns  {boolean}   true if the userID exists and deleted, false otherwise.
    */
   removeUserID(userID) {
+    let found = false;
     for (let i = 0; i < this.globalRegistryRecord.userIDs.length; i++) {
       if (this.globalRegistryRecord.userIDs == userID) {
         this.globalRegistryRecord.userIDs.splice(i, 1);
+        found = true;
       }
     }
+    return found;
   }
 
   /**
@@ -411,6 +417,14 @@ class GraphConnector {
     // TODO: reject invalid GUIDs
 
     this.contacts.push(new GraphConnectorContactData(guid, firstName, lastName));
+  }
+
+  /**
+   * Gets all the contacts.
+   * @returns  {array}   All the contacts. The format is: Contacts<GraphConnectorContactData>.
+   */
+  getAllContacts() {
+    return this.contacts;
   }
 
   /**
