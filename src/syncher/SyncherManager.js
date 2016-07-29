0adef5ef7140156ec93ddda6b6de6574ec24d25e
Robert Ende <robert.ende@fokus.fraunhofer.de>
2016-07-29 12:29:33 +0200

    improved object validation

--- src/syncher/SyncherManager.js
+++ src/syncher/SyncherManager.js
@@ -107,14 +107,22 @@ class SyncherManager {
       try {
         let obj = msg.body.value;
         let schema = descriptor.sourcePackage.sourceCode;
-        var result = tv4.validateMultiple(obj, schema);
 
-        // warn about failed object validation if validation failed or schema contains $refs
-        if (!result.valid || (result.missing && result.missing.length > 0)) {
+        // add support for schema referencing itself
+        tv4.addSchema(schema.id, schema);
+
+        // validate
+        let result = tv4.validateMultiple(obj, schema);
+
+        // delete error stacks to improve logging
+        result.errors.forEach((error) => {
+          delete error.stack;
+        });
+
+        // print more details about validation if it fails or schema contains $refs
+        if (!result.valid || (result.missing.length > 0)) {
           console.warn("Object validation " + (result.valid ? "succeeded, but schema contained references:" : "failed:"), JSON.stringify(result, null, 2));
           console.debug("Object:", JSON.stringify(obj, null, 2), "\r\nSchema:", JSON.stringify(schema, null, 2));
-
-          // TODO maybe change handling and enforce Error
         } else {
           console.log("Object validation succeeded");
         }
