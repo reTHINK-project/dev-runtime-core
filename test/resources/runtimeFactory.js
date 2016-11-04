1efd96a981f7ac10657bde580d65386da4445a20
Vitor Silva <vitorsilvaemail@gmail.com>
2016-11-04 16:09:59 +0000

    Update the syncherManager to handle with message.body.scheme; Generate new distribution files; Update the runtimeFactory with sotrageManager

--- test/resources/runtimeFactory.js
+++ test/resources/runtimeFactory.js
@@ -28,6 +28,31 @@ export const runtimeFactory = {
     return new PersistenceManager(localStorage);
   },
 
+  storageManager() {
+    return {
+      set: function(key, version, value) {
+        return new Promise((resolve) => {
+          resolve(undefined);
+        });
+      },
+      get: function(key) {
+        return new Promise((resolve) => {
+          resolve(undefined);
+        });
+      },
+      getVersion: function(key) {
+        return new Promise((resolve) => {
+          resolve(undefined);
+        });
+      },
+      delete: function(key) {
+        return new Promise((resolve) => {
+          resolve(undefined);
+        });
+      }
+    };
+  },
+
   // TODO optimize the parameter was passed to inside the RuntimeCatalogue
   createRuntimeCatalogue() {
     let _this = this;
