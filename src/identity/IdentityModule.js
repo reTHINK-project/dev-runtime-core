198e5748194927872277198f490bdaecc70f7c9c
Vitor Silva <vitorsilvaemail@gmail.com>
2016-11-17 09:44:26 +0000

    fix the choosen of environment on identity module

--- src/identity/IdentityModule.js
+++ src/identity/IdentityModule.js
@@ -422,71 +422,15 @@ class IdentityModule {
             reject('error on GUI received message.');
           }
         });
+      }).catch(error => {
+        console.log('Error on identity acquisition ', error);
+        reject(error);
+      });
 
-        _this.runtimeCapabilities.isAvailable('node').then((result) => {
-          console.log('node identity acquisition ', result);
+      _this.runtimeCapabilities.isAvailable('node').then((result) => {
+        console.log('node identity acquisition ', result);
 
-          if (!result) return;
-
-          if (_this.currentIdentity !== undefined) {
-            //TODO verify whether the token is still valid or not.
-            // should be needed to make further requests, to obtain a valid token
-            return resolve(_this.currentIdentity);
-          } else {
-            console.log('getIdentityAssertion for nodejs');
-            let randomNumber = Math.floor((Math.random() * 10000) + 1);
-            let identityBundle = {
-              assertion: 'assertion',
-              idp:'nodejs',
-              userProfile: {
-                avatar: 'https://lh3.googleusercontent.com/-WaCrjVMMV-Q/AAAAAAAAAAI/AAAAAAAAAAs/8OlVqCpSB9c/photo.jpg',
-                cn: 'test nodejs',
-                username: 'nodejs-' + randomNumber + '@nodejs.com',
-                userURL: 'user://nodejs.com/nodejs-' + randomNumber
-              }};
-            _this.currentIdentity = identityBundle;
-            _this.identities.push(identityBundle);
-            return resolve(identityBundle);
-          }
-
-        }).catch(error => {
-          console.log('Error on identity acquisition ', error);
-          reject(error);
-        });
-
-        /*try {
-        if (window) {
-
-          let identitiesInfo = _this.getIdentitiesToChoose();
-
-          _this.requestIdentityToGUI(identitiesInfo.identities, identitiesInfo.idps).then(value => {
-
-            if (value.type === 'identity') {
-
-              let chosenID = getUserURLFromEmail(value.value);
-
-              // returns the identity info from the chosen id
-              for (let i in _this.identities) {
-                if (_this.identities[i].identity === chosenID) {
-                  return resolve(_this.identities[i].messageInfo);
-                }
-              }
-              reject('no identity was found .');
-            } else if (value.type === 'idp') {
-
-              _this.callGenerateMethods(value.value, origin).then((value) => {
-                resolve(value);
-              }, (err) => {
-                reject(err);
-              });
-
-            } else {
-              reject('error on GUI received message.');
-            }
-          });
-
-        }
-      } catch (error) {
+        if (!result) return;
 
         if (_this.currentIdentity !== undefined) {
           //TODO verify whether the token is still valid or not.
@@ -508,7 +452,10 @@ class IdentityModule {
           _this.identities.push(identityBundle);
           return resolve(identityBundle);
         }
-      }*/
+
+      }).catch(error => {
+        console.log('Error on identity acquisition ', error);
+        reject(error);
       });
     });
   }
