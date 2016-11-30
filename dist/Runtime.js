a246816900fc72c9a4e3fc39cea68ba580140226
Vitor Silva <vitorsilvaemail@gmail.com>
2016-11-30 13:38:17 +0000

    update the distribution files

--- dist/Runtime.js
+++ dist/Runtime.js
@@ -23,7 +23,7 @@
 
 // Distribution file for Runtime.js 
 // version: 0.7.1
-// Last build: Wed Nov 30 2016 13:30:01 GMT+0000 (WET)
+// Last build: Wed Nov 30 2016 13:34:02 GMT+0000 (WET)
 
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.Runtime = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 (function (global){
