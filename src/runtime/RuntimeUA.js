776887c8a69c2c34f8304e97d2e6f5f56b21e665
senansharhan <senan.sharhan@hotmail.com>
2016-12-13 17:54:25 +0100

    update RuntimeUA

--- src/runtime/RuntimeUA.js
+++ src/runtime/RuntimeUA.js
@@ -51,212 +51,239 @@ import RuntimeCoreCtx from '../policy/context/RuntimeCoreCtx';
  */
 class RuntimeUA {
 
-    /**
-     * Create a new instance of Runtime User Agent
-     * @param {runtimeFactory} runtimeFactory - Specific implementation for the environment where the core runtime will run;
-     * @param {domain} domainURL - specify the domain base for the runtime;
-     */
-    constructor(runtimeFactory, domain) {
+  /**
+   * Create a new instance of Runtime User Agent
+   * @param {runtimeFactory} runtimeFactory - Specific implementation for the environment where the core runtime will run;
+   * @param {domain} domainURL - specify the domain base for the runtime;
+   */
+  constructor(runtimeFactory, domain) {
 
-        if (!runtimeFactory) throw new Error('The sandbox factory is a needed parameter');
-        if (!domain) throw new Error('You need the domain of runtime');
+    if (!runtimeFactory) throw new Error('The sandbox factory is a needed parameter');
+    if (!domain) throw new Error('You need the domain of runtime');
 
-        let _this = this;
+    let _this = this;
 
-        // Configuration object with information related with servers
-        _this.runtimeConfiguration = Object.assign({domain: domain}, runtimeConfiguration);
+    // Configuration object with information related with servers
+    _this.runtimeConfiguration = Object.assign({domain: domain}, runtimeConfiguration);
 
-        _this.runtimeFactory = runtimeFactory;
-        _this.runtimeCatalogue = runtimeFactory.createRuntimeCatalogue();
-        _this.persistenceManager = runtimeFactory.persistenceManager();
+    _this.runtimeFactory = runtimeFactory;
+    _this.runtimeCatalogue = runtimeFactory.createRuntimeCatalogue();
 
-        // Prepare the loader to load the hyperties, protostubs and idpproxy;
-        _this.loader = new Loader(_this.runtimeConfiguration);
+    if (typeof runtimeFactory.createRuntimeCatalogue === 'function') {
+      _this.persistenceManager = runtimeFactory.createRuntimeCatalogue();
+    } else {
+      throw new Error('Check your Runtime Factory because it need the Runtime Catalogue implementation');
+    }
 
-        // TODO: post and return registry/hypertyRuntimeInstance to and from Back-end Service
-        // the response is like: runtime://sp1/123
+    if (typeof runtimeFactory.persistenceManager === 'function') {
+      _this.persistenceManager = runtimeFactory.persistenceManager();
+    } else {
+      throw new Error('Check your Runtime Factory because it need the Persistence Manager implementation');
+    }
 
-        let runtimeURL = 'runtime://' + domain + '/' + Math.floor((Math.random() * 10000) + 1);
-        _this.runtimeURL = runtimeURL;
-        _this.domain = domain;
-
-        // TODO: check if runtime catalogue need the runtimeURL;
-        _this.runtimeCatalogue.runtimeURL = runtimeURL;
-
-        // Instantiate the identity Module
-        _this.identityModule = new IdentityModule(runtimeURL);
-
-        // Use the sandbox factory to create an AppSandbox;
-        // In the future can be decided by policyEngine if we need
-        // create a AppSandbox or not;
-        let appSandbox = runtimeFactory.createAppSandbox();
-
-        // Instantiate the Registry Module
-        _this.registry = new Registry(runtimeURL, appSandbox, _this.identityModule, _this.runtimeCatalogue);
-
-        // Set the loader to load Hyperties, Stubs and IdpProxies
-        _this.registry.loader = _this.loader;
-
-        // Instantiate the Message Bus
-        _this.messageBus = new MessageBus(_this.registry);
-
-        // Instantiate the Policy Engine
-        _this.policyEngine = new PEP(new RuntimeCoreCtx(_this.identityModule, _this.registry, _this.persistenceManager));
-
-        _this.messageBus.pipeline.handlers = [
-
-            // Policy message authorise
-            function(ctx) {
-                _this.policyEngine.authorise(ctx.msg).then(function(changedMgs) {
-                    ctx.msg = changedMgs;
-                    ctx.next();
-                }).catch(function(reason) {
-                    console.error(reason);
-                    ctx.fail(reason);
-                });
-            }
-        ];
-
-        // Add to App Sandbox the listener;
-        appSandbox.addListener('*', function(msg) {
-            _this.messageBus.postMessage(msg);
+    if (typeof runtimeFactory.storageManager === 'function') {
+      _this.storageManager = runtimeFactory.storageManager();
+    } else {
+      throw new Error('Check your Runtime Factory because it need the Storage Manager implementation');
+    }
+    if (typeof runtimeFactory.runtimeCapabilities === 'function') {
+      _this.runtimeCapabilities = runtimeFactory.runtimeCapabilities(_this.storageManager);
+      _this.runtimeCapabilities.getRuntimeCapabilities().then((result) => {
+          console.log('capabilities: ', result);
+        }).catch((err) => {
+          console.log('Error: ', err);
         });
-
-        // Register messageBus on Registry
-        _this.registry.messageBus = _this.messageBus;
-
-        // Register registry on IdentityModule
-        _this.identityModule.registry = _this.registry;
-
-        // Use sandbox factory to use specific methods
-        // and set the message bus to the factory
-        runtimeFactory.messageBus = _this.messageBus;
-
-        // Instanciate the SyncherManager;
-        _this.syncherManager = new SyncherManager(_this.runtimeURL, _this.messageBus, _this.registry, _this.runtimeCatalogue);
-
-        // Set into loader the needed components;
-        _this.loader.registry = _this.registry;
-        _this.loader.runtimeURL = _this.runtimeURL;
-        _this.loader.messageBus = _this.messageBus;
-        _this.loader.runtimeCatalogue = _this.runtimeCatalogue;
-        _this.loader.runtimeFactory = _this.runtimeFactory;
-
-        // Instantiate the Graph Connector
-        _this.graphConnector = new GraphConnector(_this.runtimeURL, _this.messageBus);
-
+    } else {
+      console.info('Check your RuntimeFactory because it need the Runtime Capabilities implementation');
     }
 
-    /**
-     * Accomodate interoperability in H2H and proto on the fly for newly discovered devices in M2M
-     * @param  {CatalogueDataObject.HypertyDescriptor}   descriptor    descriptor
-     */
-    discoverHiperty(descriptor) {
-        // Body...
-    }
+    // Prepare the loader to load the hyperties, protostubs and idpproxy;
+    _this.loader = new Loader(_this.runtimeConfiguration);
 
-    /**
-     * Register Hyperty deployed by the App that is passed as input parameter. To be used when App and Hyperties are from the same domain otherwise the RuntimeUA will raise an exception and the App has to use the loadHyperty(..) function.
-     * @param  {Object} Object                   hypertyInstance
-     * @param  {URL.HypertyCatalogueURL}         descriptor      descriptor
-     */
-    registerHyperty(hypertyInstance, descriptor) {
-        // Body...
-    }
+    // TODO: post and return registry/hypertyRuntimeInstance to and from Back-end Service
+    // the response is like: runtime://sp1/123
 
-    /**
-     * Deploy Hyperty from Catalogue URL
-     * @param  {URL.HypertyCatalogueURL}    hyperty hypertyDescriptor url;
-     */
-    loadHyperty(hypertyDescriptorURL) {
+    let runtimeURL = 'runtime://' + domain + '/' + Math.floor((Math.random() * 10000) + 1);
+    _this.runtimeURL = runtimeURL;
+    _this.domain = domain;
 
-        if (!hypertyDescriptorURL) throw new Error('Hyperty descriptor url parameter is needed');
+    // TODO: check if runtime catalogue need the runtimeURL;
+    _this.runtimeCatalogue.runtimeURL = runtimeURL;
 
-        return new Promise((resolve, reject) => {
+    // Instantiate the identity Module
+    _this.identityModule = new IdentityModule(runtimeURL, _this.runtimeCapabilities);
 
-            this.loader.loadHyperty(hypertyDescriptorURL)
-                .then((result) => {
-                    resolve(result);
-                })
-                .catch((reason) => {
-                    reject(reason);
-                });
+    // Use the sandbox factory to create an AppSandbox;
+    // In the future can be decided by policyEngine if we need
+    // create a AppSandbox or not;
+    let appSandbox = runtimeFactory.createAppSandbox();
 
-        });
+    // Instantiate the Registry Module
+    _this.registry = new Registry(runtimeURL, appSandbox, _this.identityModule, _this.runtimeCatalogue, _this.runtimeCapabilities);
 
-    }
+    // Set the loader to load Hyperties, Stubs and IdpProxies
+    _this.registry.loader = _this.loader;
 
-    /**
-     * Deploy Stub from Catalogue URL or domain url
-     * @param  {URL.URL}     domain          domain
-     */
-    loadStub(protostubURL) {
+    // Instantiate the Message Bus
+    _this.messageBus = new MessageBus(_this.registry);
 
-        if (!protostubURL) throw new Error('ProtoStub descriptor url parameter is needed');
+    // Instantiate the Policy Engine
+    _this.policyEngine = new PEP(new RuntimeCoreCtx(_this.identityModule, _this.registry, _this.persistenceManager));
 
-        return new Promise((resolve, reject) => {
+    _this.messageBus.pipeline.handlers = [
 
-            this.loader.loadStub(protostubURL)
-                .then((result) => {
-                    resolve(result);
-                })
-                .catch((reason) => {
-                    reject(reason);
-                });
+        // Policy message authorise
+        function(ctx) {
+            _this.policyEngine.authorise(ctx.msg).then(function(changedMgs) {
+                ctx.msg = changedMgs;
+                ctx.next();
+              }).catch(function(reason) {
+                console.error(reason);
+                ctx.fail(reason);
+              });
+          }
+    ];
 
-        });
+    // Add to App Sandbox the listener;
+    appSandbox.addListener('*', function(msg) {
+        _this.messageBus.postMessage(msg);
+      });
 
-    }
+    // Register messageBus on Registry
+    _this.registry.messageBus = _this.messageBus;
 
-    /**
-     * Deploy idpProxy from Catalogue URL or domain url
-     * @param  {URL.URL}     domain          domain
-     */
-    loadIdpProxy(idpProxyURL) {
+    // Register registry on IdentityModule
+    _this.identityModule.registry = _this.registry;
 
-        if (!idpProxyURL) throw new Error('The IDP Proxy URL is a needed parameter, could be a DOMAIN or a URL');
+    // Use sandbox factory to use specific methods
+    // and set the message bus to the factory
+    runtimeFactory.messageBus = _this.messageBus;
 
-        return new Promise((resolve, reject) => {
-            this.loader.loadIdpProxy(idpProxyURL)
-                .then((result) => {
-                    resolve(result);
-                })
-                .catch((reason) => {
-                    reject(reason);
-                });
-        });
+    // Instanciate the SyncherManager;
+    _this.syncherManager = new SyncherManager(_this.runtimeURL, _this.messageBus, _this.registry, _this.runtimeCatalogue);
 
-    }
+    // Set into loader the needed components;
+    _this.loader.registry = _this.registry;
+    _this.loader.runtimeURL = _this.runtimeURL;
+    _this.loader.messageBus = _this.messageBus;
+    _this.loader.runtimeCatalogue = _this.runtimeCatalogue;
+    _this.loader.runtimeFactory = _this.runtimeFactory;
 
-    /**
-     * Used to close all the runtime; Unregister all hyperties;
-     * @return {Promise<Boolean>} result of the close method, with true or false to the operation success;
-     */
-    close() {
-        let _this = this;
+    // Instantiate the Graph Connector
+    _this.graphConnector = new GraphConnector(_this.runtimeURL, _this.messageBus);
 
-        console.info('Unregister all hyperties');
-        return new Promise(function(resolve, reject) {
+  }
 
-            _this.registry.unregisterAllHyperties().then(function(result) {
-                console.info('All the hyperties are unregisted with Success:', result);
-                resolve(true);
-            }).catch(function(reason) {
-                console.error('Failed to unregister the hyperties', reason);
-                reject(false);
-            });
+  /**
+   * Accomodate interoperability in H2H and proto on the fly for newly discovered devices in M2M
+   * @param  {CatalogueDataObject.HypertyDescriptor}   descriptor    descriptor
+   */
+  discoverHiperty(descriptor) {
+    // Body...
+  }
 
-        });
+  /**
+   * Register Hyperty deployed by the App that is passed as input parameter. To be used when App and Hyperties are from the same domain otherwise the RuntimeUA will raise an exception and the App has to use the loadHyperty(..) function.
+   * @param  {Object} Object                   hypertyInstance
+   * @param  {URL.HypertyCatalogueURL}         descriptor      descriptor
+   */
+  registerHyperty(hypertyInstance, descriptor) {
+    // Body...
+  }
 
-    }
+  /**
+   * Deploy Hyperty from Catalogue URL
+   * @param  {URL.HypertyCatalogueURL}    hyperty hypertyDescriptor url;
+   */
+  loadHyperty(hypertyDescriptorURL) {
 
-    /**
-     * Used to check for updates about components handled in the Catalogue including protocol stubs and Hyperties. check relationship with lifecycle management provided by Service Workers
-     * @param  {CatalogueURL}       url url
-     */
-    checkForUpdate(url) {
-        // Body...
-    }
+    if (!hypertyDescriptorURL) throw new Error('Hyperty descriptor url parameter is needed');
+
+    return new Promise((resolve, reject) => {
+
+        this.loader.loadHyperty(hypertyDescriptorURL)
+            .then((result) => {
+                resolve(result);
+              })
+            .catch((reason) => {
+                reject(reason);
+              });
+
+      });
+
+  }
+
+  /**
+   * Deploy Stub from Catalogue URL or domain url
+   * @param  {URL.URL}     domain          domain
+   */
+  loadStub(protostubURL) {
+
+    if (!protostubURL) throw new Error('ProtoStub descriptor url parameter is needed');
+
+    return new Promise((resolve, reject) => {
+
+        this.loader.loadStub(protostubURL)
+            .then((result) => {
+                resolve(result);
+              })
+            .catch((reason) => {
+                reject(reason);
+              });
+
+      });
+
+  }
+
+  /**
+   * Deploy idpProxy from Catalogue URL or domain url
+   * @param  {URL.URL}     domain          domain
+   */
+  loadIdpProxy(idpProxyURL) {
+
+    if (!idpProxyURL) throw new Error('The IDP Proxy URL is a needed parameter, could be a DOMAIN or a URL');
+
+    return new Promise((resolve, reject) => {
+        this.loader.loadIdpProxy(idpProxyURL)
+            .then((result) => {
+                resolve(result);
+              })
+            .catch((reason) => {
+                reject(reason);
+              });
+      });
+
+  }
+
+  /**
+   * Used to close all the runtime; Unregister all hyperties;
+   * @return {Promise<Boolean>} result of the close method, with true or false to the operation success;
+   */
+  close() {
+    let _this = this;
+
+    console.info('Unregister all hyperties');
+    return new Promise(function(resolve, reject) {
+
+        _this.registry.unregisterAllHyperties().then(function(result) {
+            console.info('All the hyperties are unregisted with Success:', result);
+            resolve(true);
+          }).catch(function(reason) {
+            console.error('Failed to unregister the hyperties', reason);
+            reject(false);
+          });
+
+      });
+
+  }
+
+  /**
+   * Used to check for updates about components handled in the Catalogue including protocol stubs and Hyperties. check relationship with lifecycle management provided by Service Workers
+   * @param  {CatalogueURL}       url url
+   */
+  checkForUpdate(url) {
+    // Body...
+  }
 
 }
 
