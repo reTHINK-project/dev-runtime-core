882cc400e96ea1d8a8332b439af9587a3e60a409
Bernardo Marques Graça <bernardo.marquesg@gmail.com>
2017-05-04 17:56:58 +0100

    sm url fix

--- dist/minibus.js
+++ dist/minibus.js
@@ -1,5 +1,5 @@
 // version: 0.7.1
-// date: Thu May 04 2017 15:18:57 GMT+0100 (WEST)
+// date: Thu May 04 2017 17:56:31 GMT+0100 (WEST)
 // licence: 
 /**
 * Copyright 2016 PT Inovação e Sistemas SA
