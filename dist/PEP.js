6f7ce46377452184e7d0ceff06548f50efbafce7
Bernardo Marques Graça <bernardo.marquesg@gmail.com>
2017-05-09 15:29:16 +0100

    Messages fix new specs

--- dist/PEP.js
+++ dist/PEP.js
@@ -1,5 +1,5 @@
 // version: 0.7.1
-// date: Mon May 08 2017 12:01:54 GMT+0100 (WEST)
+// date: Tue May 09 2017 15:28:34 GMT+0100 (WEST)
 // licence: 
 /**
 * Copyright 2016 PT Inovação e Sistemas SA
