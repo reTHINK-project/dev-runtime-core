f9f75fd6359c1fe668749e6e44d583512f01519c
luistduarte <lduarte.suil@gmail.com>
2017-06-27 10:38:56 +0100

    1fix

--- dist/sandbox.js
+++ dist/sandbox.js
@@ -1,5 +1,5 @@
 // version: 0.8.1
-// date: Tue Jun 27 2017 10:24:41 GMT+0100 (WEST)
+// date: Tue Jun 27 2017 10:37:12 GMT+0100 (WEST)
 // licence: 
 /**
 * Copyright 2016 PT Inovação e Sistemas SA
