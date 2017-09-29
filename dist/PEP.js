3f41d438bf87099bdb795703a010d6218ebe0ba3
pchainho <pchainho@gmail.com>
2017-09-29 18:49:35 +0100

    Dist updated

--- dist/PEP.js
+++ dist/PEP.js
@@ -1,5 +1,5 @@
 // version: 0.9.1
-// date: Mon Sep 04 2017 18:26:51 GMT+0100 (GMT Daylight Time)
+// date: Fri Sep 29 2017 12:20:41 GMT+0100 (GMT Daylight Time)
 // licence: 
 /**
 * Copyright 2016 PT Inovação e Sistemas SA
@@ -26,7 +26,7 @@
 
 
 // version: 0.9.1
-// date: Mon Sep 04 2017 18:26:51 GMT+0100 (GMT Daylight Time)
+// date: Fri Sep 29 2017 12:20:41 GMT+0100 (GMT Daylight Time)
 // licence: 
 /**
 * Copyright 2016 PT Inovação e Sistemas SA
