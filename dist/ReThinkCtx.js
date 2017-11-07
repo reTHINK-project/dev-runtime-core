f4f833400ad7df28c8aaae5202a07c1249cd3217
luistduarte <lduarte.suil@gmail.com>
2017-11-07 11:55:04 +0000

    update dist

--- dist/ReThinkCtx.js
+++ dist/ReThinkCtx.js
@@ -1,5 +1,5 @@
 // version: 0.9.1
-// date: Tue Nov 07 2017 09:39:58 GMT+0000 (WET)
+// date: Tue Nov 07 2017 11:54:24 GMT+0000 (WET)
 // licence: 
 /**
 * Copyright 2016 PT Inovação e Sistemas SA
@@ -26,7 +26,7 @@
 
 
 // version: 0.9.1
-// date: Tue Nov 07 2017 09:39:58 GMT+0000 (WET)
+// date: Tue Nov 07 2017 11:54:24 GMT+0000 (WET)
 // licence: 
 /**
 * Copyright 2016 PT Inovação e Sistemas SA
