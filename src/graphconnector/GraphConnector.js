8762b4913c6ef4bd97e7ce230ca83fce50fdfb31
Vitor Silva <vitor-t-silva@telecom.pt>
2017-11-08 12:29:02 +0000

    Prepare release 0.10.0
    - Update the package.json to use service-framework V0.7.2
    - Update the documentation
    - Generate new distribution files

--- src/graphconnector/GraphConnector.js
+++ src/graphconnector/GraphConnector.js
@@ -1,10 +1,22 @@
-let GraphConnector;
-if (process.env.MODE !== 'light') {
-  GraphConnector = require('./GraphConnectorBase');
-  if (GraphConnector && GraphConnector.hasOwnProperty('default')) GraphConnector = GraphConnector.default;
-} else {
-  GraphConnector = require('./GraphConnectorLight');
-  if (GraphConnector &&  GraphConnector.hasOwnProperty('default')) GraphConnector = GraphConnector.default;
-}
+// let GraphConnector;
+// if (process.env.MODE !== 'light') {
+//   GraphConnector = require('./GraphConnectorBase');
+//   if (GraphConnector && GraphConnector.hasOwnProperty('default')) GraphConnector = GraphConnector.default;
+// } else {
+//   GraphConnector = require('./GraphConnectorLight');
+//   if (GraphConnector &&  GraphConnector.hasOwnProperty('default')) GraphConnector = GraphConnector.default;
+// }
+
+/**
+ * The GraphConnectorBase have some problems with the size;
+ * @deprecated Due to some problems increase a lot the file size
+ */
+// import GraphConnector from './GraphConnectorBase';
+
+/**
+ *
+ * @experimental used instead of GraphConnectorBase;
+ */
+import GraphConnector from './GraphConnectorLight';
 
 export default GraphConnector;
