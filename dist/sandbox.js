252a2cecf7baa56e8e703840ea8e4c98be2f778a
suil <lduarte.suil@gmail.com>
2018-02-02 14:18:48 +0000

    new dist

--- dist/sandbox.js
+++ dist/sandbox.js
@@ -1,5 +1,5 @@
 // version: 0.10.2
-// date: Fri Feb 02 2018 13:55:28 GMT+0000 (WET)
+// date: Fri Feb 02 2018 14:17:47 GMT+0000 (WET)
 // licence: 
 /**
 * Copyright 2016 PT Inovação e Sistemas SA
@@ -26,7 +26,7 @@
 
 
 // version: 0.10.2
-// date: Fri Feb 02 2018 13:55:28 GMT+0000 (WET)
+// date: Fri Feb 02 2018 14:17:47 GMT+0000 (WET)
 // licence: 
 /**
 * Copyright 2016 PT Inovação e Sistemas SA
