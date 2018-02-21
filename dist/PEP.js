80024f07526d4070ec1f6a804048581f0ea15d86
suil <lduarte.suil@gmail.com>
2018-02-21 15:30:54 +0000

    using develop, new dist

--- dist/PEP.js
+++ dist/PEP.js
@@ -1,5 +1,5 @@
 // version: 0.10.2
-// date: Wed Feb 21 2018 13:22:01 GMT+0000 (WET)
+// date: Wed Feb 21 2018 15:30:26 GMT+0000 (WET)
 // licence: 
 /**
 * Copyright 2016 PT Inovação e Sistemas SA
@@ -26,7 +26,7 @@
 
 
 // version: 0.10.2
-// date: Wed Feb 21 2018 13:22:01 GMT+0000 (WET)
+// date: Wed Feb 21 2018 15:30:26 GMT+0000 (WET)
 // licence: 
 /**
 * Copyright 2016 PT Inovação e Sistemas SA
