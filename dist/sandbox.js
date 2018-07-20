478d4d51141294a7dc7680d2af4967c11c8ab1d2
suil <lduarte.suil@gmail.com>
2018-07-20 09:00:26 +0100

    new dist

--- dist/sandbox.js
+++ dist/sandbox.js
@@ -1,5 +1,5 @@
 // version: 0.12.0
-// date: Fri Jul 20 2018 08:56:58 GMT+0100 (Western European Summer Time)
+// date: Fri Jul 20 2018 08:59:57 GMT+0100 (Western European Summer Time)
 // licence: 
 /**
 * Copyright 2016 PT Inovação e Sistemas SA
@@ -26,7 +26,7 @@
 
 
 // version: 0.12.0
-// date: Fri Jul 20 2018 08:56:58 GMT+0100 (Western European Summer Time)
+// date: Fri Jul 20 2018 08:59:57 GMT+0100 (Western European Summer Time)
 // licence: 
 /**
 * Copyright 2016 PT Inovação e Sistemas SA
