2cc0078e31bf1f9cde6f7a5d4ded359049ceaadc
Paulo Chainho <pchainho@gmail.com>
2018-08-20 18:03:39 +0100

    SandboxFactory: added RegistrationStatus creation

--- src/sandbox/SandboxFactory.js
+++ src/sandbox/SandboxFactory.js
@@ -86,6 +86,12 @@ class SandboxFactory {
      return this._divideURL;
    }
 
+   createRegistrationStatus(observer, runtimeURL, chatUrl, bus) {
+     return new RegistrationStatus(observer, runtimeURL, chatUrl, bus);
+
+   }
+
+
 
 }
 
