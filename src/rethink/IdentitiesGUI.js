3cd7afa4aade8179ee35be999ad07ed813b6945c
Paulo Chainho <pchainho@gmail.com>
2018-11-17 11:30:05 +0000

    rethink: added missing functions from runtimeBrowser including authorise

--- src/rethink/IdentitiesGUI.js
+++ src/rethink/IdentitiesGUI.js
@@ -13,12 +13,14 @@ class IdentitiesGUI {
 
     _this._messageBus.addListener(guiURL, msg => {
       let funcName = msg.body.method;
+      console.log('[IdentitiesGUI.listener] received msg: ' + msg);
 
       if (funcName === 'openPopup') {
         let urlreceived = msg.body.params.urlreceived;
+
         _this._openPopup(urlreceived).then((returnedValue) => {
-          let value = {type: 'execute', value: returnedValue, code: 200};
-          let replyMsg = {id: msg.id, type: 'response', to: msg.from, from: msg.to, body: value};
+          let value = { type: 'execute', value: returnedValue, code: 200 };
+          let replyMsg = { id: msg.id, type: 'response', to: msg.from, from: msg.to, body: value };
           _this._messageBus.postMessage(replyMsg);
         });
         return;
@@ -26,13 +28,54 @@ class IdentitiesGUI {
     });
   }
 
+  callIdentityModuleFunc(methodName, parameters) {
+    let _this = this;
+
+    return new Promise((resolve, reject) => {
+      const message = {
+        type: 'execute', to: _this._idmURL, from: _this._guiURL,
+        body: { resource: 'identity', method: methodName, params: parameters }
+      };
+
+      this._messageBus.postMessage(message, (res) => {
+
+        if (res.body.code < 299) {
+          let result = res.body.value;
+          resolve(result);
+        } else {
+          resolve(res.body);
+        }
+      });
+
+    });
+  }
+
   _openPopup(urlreceived) {
 
     return new Promise((resolve, reject) => {
+      console.log('[IdentitiesGUI._openPopup] url: ' + urlreceived);
+      function wait(ms) {
+        var start = new Date().getTime();
+        var end = start;
+        while (end < start + ms) {
+          end = new Date().getTime();
+        }
+      }
 
-      let win = window.open(urlreceived, 'openIDrequest', 'width=800, height=600');
+      let win;
+      if (!urlreceived) {
+        win = window.open('', 'openIDrequest', 'location=1,status=1');
+        this.win = win;
+        resolve();
+      } else {
+        wait(1000);
+        win = this.win;
+        win.location.href = urlreceived;
+      }
+
+//      let win = window.open(urlreceived, 'openIDrequest', 'width=800, height=600');
       if (window.cordova) {
-        win.addEventListener('loadstart', function(e) {
+        win.addEventListener('loadstart', function (e) {
           let url = e.url;
           let code = /\&code=(.+)$/.exec(url);
           let error = /\&error=(.+)$/.exec(url);
@@ -45,19 +88,22 @@ class IdentitiesGUI {
           }
         });
       } else {
-        let pollTimer = setInterval(function() {
+        let pollTimer = setInterval(function () {
           try {
             if (win.closed) {
-              return reject('Some error occured when trying to get identity.');
+//              return reject('Some error occured when trying to get identity.');
               clearInterval(pollTimer);
             }
 
-            if (win.document.URL.indexOf('id_token') !== -1 || win.document.URL.indexOf(location.origin) !== -1) {
-              window.clearInterval(pollTimer);
-              let url =   win.document.URL;
+//            if (win.document.URL.indexOf('id_token') !== -1 || win.document.URL.indexOf(location.origin) !== -1) {
+            if ( win.document.URL.indexOf(location.origin) !== -1) {
+                window.clearInterval(pollTimer);
+              let url = win.document.URL;
 
-              win.close();
-              return resolve(url);
+//              win.close();
+//              return resolve(url);
+              resolve(url);
+              return win.close();
             }
           } catch (e) {
             //return reject('openPopup error 2 - should not happen');
@@ -68,6 +114,119 @@ class IdentitiesGUI {
     });
   }
 
+  authorise(idp, resource) {
+
+
+    return this._openPopup()
+      .then((res) => {
+        const data = { scope: resource, idpDomain: idp };
+        return this.callIdentityModuleFunc('getAccessTokenAuthorisationEndpoint', data);
+      })
+      .then((value) => {
+        console.log('[IdentitiesGUI.authorise] receivedURL from idp Proxy: ' + value);
+
+        return this._openPopup(value);
+      }).then((result) => {
+
+        console.log('[IdentitiesGUI.authorise.openPopup.result]', result);
+
+        // resource as array
+
+
+        const data = { resources: [resource], idpDomain: idp, login: result };
+        return this.callIdentityModuleFunc('getAccessToken', data);
+      }).then((result) => {
+
+        if (result.hasOwnProperty('code') && result.code > 299) {
+          console.error('[IdentitiesGUI.authorise.getAccessToken] error', result);
+          return (result);
+
+        } else {
+          console.log('[IdentitiesGUI.authorise.getAccessToken.result]', result);
+          return this.callIdentityModuleFunc('addAccessToken', result);
+        }
+
+      }).then((value) => {
+//        this._drawer.open = false;
+        return value;
+      });
+
+  }
+
+  loginWithIDP(idp) {
+
+    let _publicKey;
+
+    return this._openPopup()
+      .then((result) => {
+        return this.callIdentityModuleFunc('getMyPublicKey', {});
+      }).then((publicKey) => {
+        _publicKey = publicKey;
+        const data = { contents: publicKey, origin: 'origin', usernameHint: undefined, idpDomain: idp };
+        return this.callIdentityModuleFunc('sendGenerateMessage', data);
+      })
+      .then((value) => {
+
+        console.log('[IdentitiesGUI.loginWithIDP] received reply to request for Login URL from idp Proxy: ' + value + '...');
+
+        if (value.hasOwnProperty('description') && value.description.hasOwnProperty('loginUrl')) {
+          let url = value.description.loginUrl;
+          let finalURL;
+
+          //check if the receivedURL contains the redirect field and replace it
+          if (url.indexOf('redirect_uri') !== -1) {
+            let firstPart = url.substring(0, url.indexOf('redirect_uri'));
+            let secondAuxPart = url.substring(url.indexOf('redirect_uri'), url.length);
+
+            let secondPart = secondAuxPart.substring(secondAuxPart.indexOf('&'), url.length);
+
+            //check if the reddirect field is the last field of the URL
+            if (secondPart.indexOf('&') !== -1) {
+              finalURL = firstPart + 'redirect_uri=' + location.origin + secondPart;
+            } else {
+              finalURL = firstPart + 'redirect_uri=' + location.origin;
+            }
+          }
+
+          this.resultURL = finalURL || url;
+
+          console.log('[IdentitiesGUI.openPopup]', this.resultURL);
+          return this.openPopup(this.resultURL);
+        }
+
+
+      }).then((identity) => {
+
+        console.log('[IdentitiesGUI.loginWithIDP] identity', identity);
+
+        const data = { contents: _publicKey, origin: 'origin', usernameHint: identity, idpDomain: idp };
+        return this.callIdentityModuleFunc('sendGenerateMessage', data);
+      }).then((result) => {
+
+        console.log('[IdentitiesGUI.loginWithIDP] sendGenerateMessage.result', result);
+        return this.callIdentityModuleFunc('addAssertion', result);
+      }).then((value) => {
+
+        this._drawer.open = false;
+        const userURL = { type: 'identity', value: value.userProfile.userURL };
+        // const userIdentity = {type: 'identity', value: value.userProfile};
+
+        console.log('[IdentitiesGUI.loginWithIDP] final identity ', value);
+        //      this._alreadyLogin = true;
+        return userURL;
+        // return userIdentity;
+      });
+
+  }
+  logOut() {
+    let _this = this;
+    console.log('[IdentitiesGUI.logOut]');
+    return new Promise((resolve, reject) => {
+      //      this._alreadyLogin = false;
+
+      resolve(true);
+    });
+  }
 }
 
 export default IdentitiesGUI;
--- src/rethink/rethink.js
+++ src/rethink/rethink.js
@@ -42,99 +42,211 @@ const runtimeDescriptor = {
   "dataObjects": [],
   "signature": "",
   "accessControlPolicy": "somePolicy"
-  };
+};
 
 const rethink = {
 
-  install: function({domain, runtimeURL, development}={}) {
+  install: function ({ domain, runtimeURL, development } = {}) {
 
     return new Promise((resolve, reject) => {
 
-        let runtime = new RuntimeUA(runtimeDescriptor, runtimeFactory, domain);
+      let runtime = new RuntimeUA(runtimeDescriptor, runtimeFactory, domain);
 
-        window.runtime = runtime;
+      window.runtime = runtime;
 
-        runtime.init().then((result) => {
+      runtime.init().then((result) => {
 
-          let idmGuiURL = runtime.identityModule._runtimeURL + '/identity-gui';
-          let idmURL = runtime.identityModule._runtimeURL + '/idm';
-          let messageBus = runtime.messageBus;
-          let identitiesGUI = new IdentitiesGUI(idmGuiURL, idmURL, messageBus);
-          console.log('identitiesGUI: ', identitiesGUI);
+        let idmGuiURL = runtime.identityModule._runtimeURL + '/identity-gui';
+        let idmURL = runtime.identityModule._runtimeURL + '/idm';
+        let messageBus = runtime.messageBus;
+        let identitiesGUI = new IdentitiesGUI(idmGuiURL, idmURL, messageBus);
+        console.log('identitiesGUI: ', identitiesGUI);
 
-          let runtimeProxy = {
+        let runtimeProxy = {
 
-          requireHyperty: function(hyperty) {
+          requireHyperty: function (hyperty) {
 
-          return new Promise(function(resolve, reject) {
+            return new Promise(function (resolve, reject) {
 
-            //TODO: Work the message errors, probably use message factory
-            runtime.loadHyperty(hyperty, true).then(function(result) {
+              //TODO: Work the message errors, probably use message factory
+              runtime.loadHyperty(hyperty, true).then(function (result) {
 
-              let hypertyComponent = window.components[result.runtimeHypertyURL];
-              let hyperty = {
-                runtimeHypertyURL: result.runtimeHypertyURL,
-                status: result.status,
-                instance: hypertyComponent.instance,
-                name: hypertyComponent.name
+                let hypertyComponent = window.components[result.runtimeHypertyURL];
+                let hyperty = {
+                  runtimeHypertyURL: result.runtimeHypertyURL,
+                  status: result.status,
+                  instance: hypertyComponent.instance,
+                  name: hypertyComponent.name
+                };
+
+                resolve(hyperty);
+              }).catch(function (reason) {
+                reject(reason);
+              });
+
+            });
+          },
+
+          requireProtostub: function (domain) {
+            return new Promise(function (resolve, reject) {
+
+              //TODO: Work the message errors, probably use message factory
+              runtime.loadStub(domain).then(function (result) {
+
+                let protostubURL = result.url || result.runtimeProtoStubURL;
+                let protostubComponent = window.components[protostubURL];
+                let protostub = {
+                  runtimeProtostubURL: protostubURL,
+                  status: result.status,
+                  instance: protostubComponent.instance,
+                  name: protostubComponent.name
+                };
+
+                resolve(protostub);
+              }).catch(function (reason) {
+                reject(reason);
+              });
+
+            });
+          },
+
+          authorise: (idp, scope) => {
+
+            return new Promise((resolve, reject) => {
+              identitiesGUI.authorise(idp, scope).then((result) => {
+                if (result.hasOwnProperty('accessToken')) {
+                  console.log('[rethink.authorise] authorised: ', result);
+
+                  resolve(result);
+
+                  //                event.source.postMessage({ to: 'runtime:not-authorised', body: JSON.stringify(result) }, '*');
+                } else {
+                  console.warn('[rethink.authorise] not authorised: ', result);
+
+                  reject(result);
+                  //                event.source.postMessage({ to: 'runtime:authorised', body: JSON.stringify(result) }, '*');
+                }
+              });
+
+              /*            let loaded = (e) => {
+                            console.log('[RuntimeBrowser.RuntimeUAStub.Authorise] reply:', e.data);
+                            if (e.data.to === 'runtime:authorised') {
+                              window.removeEventListener('message', loaded);
+                    
+                              resolve(e.data.body);
+                            } else if (e.data.to === 'runtime:not-authorised') {
+                              window.removeEventListener('message', loaded);
+                    
+                              console.error('[RuntimeBrowser.RuntimeUAStub.Authorise] Error:', e.data);
+                              reject(e.data.body);
+                            }
+                          };
+                          window.addEventListener('message', loaded);
+                          console.log('Authorising IDP ', idp, ' with scope ', scope);
+                          iframe.contentWindow.postMessage({ to: 'core:authorise', body: { idp: idp, scope: scope } }, '*');*/
+            });
+
+          },
+
+          login: (idp) => {
+
+            return new Promise((resolve, reject) => {
+
+              identitiesGUI.loginWithIDP(idp).then((result) => {
+                //              event.source.postMessage({ to: 'runtime:loggedIn', body: result }, '*');
+                console.log('[rethink.login] loggedin: ', result);
+
+                resolve(result);
+              });
+
+              /*            let loaded = (e) => {
+                if (e.data.to === 'runtime:loggedIn') {
+                  window.removeEventListener('message', loaded);
+                  resolve(e.data.body);
+                }
               };
-    
-              resolve(hyperty);
-            }).catch(function(reason) {
-              reject(reason);
+              window.addEventListener('message', loaded);
+              console.log('Logging with IDP: ', idp);
+              iframe.contentWindow.postMessage({ to: 'core:login', body: { idp: idp } }, '*');*/
             });
 
-          });
-        },
+          },
 
-        requireProtostub: function (domain) {
-          return new Promise(function(resolve, reject) {
-
-            //TODO: Work the message errors, probably use message factory
-            runtime.loadStub(domain).then(function(result) {
-
-              let protostubURL = result.url || result.runtimeProtoStubURL;
-              let protostubComponent = window.components[protostubURL];
-              let protostub = {
-                runtimeProtostubURL: protostubURL,
-                status: result.status,
-                instance: protostubComponent.instance,
-                name: protostubComponent.name
+          listenShowAdmin: () => {
+            return new Promise((resolve, reject) => {
+              let loaded = (e) => {
+                if (e.data.to === 'runtime:gui-manager') {
+                  if (e.data.body.method === 'tokenExpired') {
+                    window.removeEventListener('message', loaded);
+                    resolve(true);
+                  }
+                }
               };
-    
-              resolve(protostub);
-            }).catch(function(reason) {
-              reject(reason);
+              window.addEventListener('message', loaded);
             });
+          },
 
-          });
-        },
+          reset: function () {
 
-        reset: function () {
+            return new Promise(function (resolve, reject) {
 
-          return new Promise(function(resolve, reject) {
+              //TODO: Work the message errors, probably use message factory
+              runtime.reset().then(function (result) {
 
-            //TODO: Work the message errors, probably use message factory
-            runtime.reset().then(function(result) {
+                resolve(result);
+              }).catch(function (reason) {
+                reject(reason);
+              });
 
-              resolve(result);
-            }).catch(function(reason) {
-              reject(reason);
             });
+          },
 
-          });
+          close: (logOut = false) => {
+            console.log('[rethink.close] logout: ', logOut);
+            return new Promise((resolve, reject) => {
+              runtime.close(logOut).then((result) => {
+                //              event.source.postMessage({ to: 'runtime:runtimeClosed', body: result }, '*');
+                //  send logout
+                if (logOut) {
+                  identitiesGUI.logOut().then((result) => {
+                    console.log('[rethink.close] closed: ', result);
+
+                    resolve(result);
+                    });
+                } else {
+                  console.log('[rethink.close] closed: ', result);
+
+                  resolve(result);
+                }
+              })
+                .catch((result) => {
+                  console.log('[rethink.close] closed: ', result);
+
+                  resolve(result);
+                });
+
+              /*            let loaded = (e) => {
+                            if (e.data.to === 'runtime:runtimeClosed') {
+                              window.removeEventListener('message', loaded);
+                              resolve(resolve(e.data.body));
+                            }
+                          };
+                          window.addEventListener('message', loaded);
+                          iframe.contentWindow.postMessage({ to: 'core:close', body: { logOut: logOut } }, '*');*/
+            });
+          }
+
         }
-      }
 
-          resolve(runtimeProxy);
-        });
+        resolve(runtimeProxy);
+      });
 
-      })
-      .catch(function(reason) {
+    })
+      .catch(function (reason) {
         console.error(reason);
         reject(reason);
       });
-//    });
+    //    });
 
   }
 
