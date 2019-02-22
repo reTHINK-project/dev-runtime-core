f3b124ea537a469ffdab84689964e7e838d7af88
Paulo Chainho <pchainho@gmail.com>
2019-02-22 12:19:22 +0000

    SyncStorageManager: added getVersion and tests

--- src/storage-manager/SyncStorageManager.js
+++ src/storage-manager/SyncStorageManager.js
@@ -44,32 +44,45 @@ class SyncStorageManager {
    * If an entry for the given key is already stored in the database, it will be updated.
    * @param {!string} key - key that can be used with {@link storageManager.get} to retrieve the value object
    * @param value - value stored in the database that is mapped to given key
-   * @returns {Promise} result - Promise that will be fulfilled with the key if the entry was stored successfully,
+   * @returns {Promise} result - Promise that will be fulfilled with the stored doc if the entry was stored successfully,
    * otherwise it is rejected with an error.
    * @memberof StorageManager
    */
 
-  set(key, value) {
+  set(key, value, version) {
     if (!key) throw Error('[SyncStorageManager.set] key is mandatory param');
     if (!value) throw Error('[SyncStorageManager.set] value is mandatory param');
 
     return new Promise((resolve, reject) => {
-      log.info('[SyncStorageManager.set] key ', key, ' value ', value);
+      console.log('[SyncStorageManager.set] key ', key, ' value ', value);
 
       value._id = key;
 
       this.db.get(key).then((doc) => {
         let data = typeof value === 'string' ? { value: value } : value;
+
+        // if no version is asked to set, it is assumed to update the same version of the document
+        // in order to avoid the increase of storage quota used
+        doc._rev = version ? version : doc._rev;
+
         Object.assign(doc, data);
         this.db.put(doc).then(() => {
           resolve(doc);
 
         });
 
-      }, () => {
-        this.db.put(value).then(() => {
-          resolve(value);
-        });
+      }, (err) => {
+        console.error('[SyncStorageManager.set] ', err);
+        if (err.name === 'not_found') {
+          let opts = {};
+          if (version) {
+            value._rev = version;
+            opts.new_edits = false;
+          } 
+          this.db.put(value, opts).then(() => {
+            resolve(value);
+          },(err) => {reject(err); } );
+        } else reject(err);
 
       });
     });
@@ -94,16 +107,18 @@ class SyncStorageManager {
       if (!value) {
         this.db.get(key).then((doc)=>{
           resolve(doc);
-        }, ()=>{
-          resolve(undefined)
+        }, (err)=>{
+          if (err.name === 'not_found') resolve(undefined)
+          else reject(err);
         });
       } 
       else this.db.get(key).then((doc) => {
         log.info('[SyncStorageManager.get] retrieved doc ', doc);
         resolve(doc[value]);
-      }, ()=>{
-        resolve(undefined)
-      });
+      }, (err)=>{
+        if (err.name === 'not_found') resolve(undefined)
+        else reject(err);
+    });
     });
 
   }
@@ -112,7 +127,7 @@ class SyncStorageManager {
    * Delete a entry from the database for a given key or the full DB in case the key is not provided.
    * @param {!string} key - key that was stored using {@link storageManager.set}
    * @param {!any} value - the value which sould be used to find the storage resource
-   * @returns {Promise} result - Promise that will be fulfilled with the number of affected rows.
+   * @returns {Promise} result - Promise that will be just fulfilled with no values if something was deleted otherwise it is fullfilled with undefined.
    * @memberof StorageManager
    */
 
@@ -131,8 +146,38 @@ class SyncStorageManager {
           delete doc[value];
           resolve(this.db.put(doc));
         }
-      }, ()=>{resolve(undefined)});
+      }, (err)=>{
+        if (err.name === 'not_found') resolve(undefined)
+        else reject(err);
+      });
     });
+  }
+
+  /**
+   * Get the version from the database for a given key.
+   * If no entry is found undefined is returned.
+   * @param {!string} key - key that was stored using {@link storageManager.set}
+   * @param {any} value - the value which sould be used to find the storage resource
+   * @param {!string} table - table which should be looking for
+   * @returns {Promise} result - Promise that will be fulfilled with the version. If not found it is fullfilled with undefined
+   * @memberof StorageManager
+   */
+  getVersion(key) {
+    if (!key) throw Error('[SyncStorageManager.get] key is mandatory param');
+
+
+    return new Promise((resolve, reject) => {
+
+      log.info('[SyncStorageManager.getVersion] for key ', key);
+
+        this.db.get(key).then((doc)=>{
+          resolve(doc._rev);
+        }, (err)=>{
+          if (err.name === 'not_found') resolve(undefined)
+          else reject(err);
+        });
+    });
+
   }
 
 
